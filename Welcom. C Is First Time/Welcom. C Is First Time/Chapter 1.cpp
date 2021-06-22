40e44d9448492697720373fef2763d941423d90e
Lee Si Young <66060749+Shung-2@users.noreply.github.com>
2021-06-22 16:27:23 +0900

    210622 study content

--- Welcom. C Is First Time/Welcom. C Is First Time/Chapter 1.cpp
+++ Welcom. C Is First Time/Welcom. C Is First Time/Chapter 1.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {	
 	// Page 28
 	cout << "Hello World!" << endl;
+	cout << endl;
+
+	// Page 42
+	string s1 = "Good";
+	string s2 = "Moring";
+	string s3 = s1 + " " + s2 + "!";
+	cout << s3 << endl;
+
 	return 0;
 }
