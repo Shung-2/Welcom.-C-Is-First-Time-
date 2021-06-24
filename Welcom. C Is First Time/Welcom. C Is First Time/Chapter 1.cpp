07780e870e9ca4097f1e970c43d7d93c1589f338
Lee Si Young <66060749+Shung-2@users.noreply.github.com>
2021-06-24 09:05:31 +0900

    210623, 24 study content
    cout, cin, string 문자열을 이용한 이름 입,출력

--- Welcom. C Is First Time/Welcom. C Is First Time/Chapter 1.cpp
+++ Welcom. C Is First Time/Welcom. C Is First Time/Chapter 1.cpp
@@ -13,6 +13,14 @@ int main()
 	string s2 = "Moring";
 	string s3 = s1 + " " + s2 + "!";
 	cout << s3 << endl;
+	cout << endl;
+
+	// Page 47
+	string name;
+	cout << "이름을 입력하시오 : ";
+	cin >> name;
+	cout << name << "을 환영합니다." << endl;
+	cout << endl;
 
 	return 0;
 }
